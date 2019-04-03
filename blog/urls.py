d720d08bd533e449219c6d4c24c28cf1f12f644a
gabi <gabiminohara@gmail.com>
2019-04-03 12:56:27 -0300

    erro

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path 
 from . import views
-from django.urls import path
-from .views import redirect_view
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('/redirect/', redirect_view),
 ]
 
 
